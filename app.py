a5585e38cf4e67664381cf2975fbb205580fe0fe
maryamzafar1 <30714111+maryamzafar1@users.noreply.github.com>
2017-08-08 19:40:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ def makeWebhookResult(data):
           {
              "title":row_title[0],
               "subtitle":row_location[0]+"/nPrice: Rs."
-              "subtitle": row_price[0],
+              "subtitle":row_price[0],
                 "item_url": "https://www.aarz.pk/property-detail/"+row_slug[0],               
                "image_url":"https://www.aarz.pk/"+row_image[0] ,
              "buttons":[
