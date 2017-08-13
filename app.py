54f3dce4065af55ac089afc8510939dac2a9872d
asharmehmood <30666174+asharmehmood@users.noreply.github.com>
2017-08-13 21:25:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def processRequest(req):
 	s_id=processsession(req)
 	maximum_value=convertMaximum(maximum_valu, price_unit)
 	print(maximum_value)
-
+	print("this is session id:",s_id)
 	#baseurl = "https://aarz.pk/bot/index.php?city_name="+city_names+"&sector_name="+sector_names+"&minPrice="+maximum_value+"&type="+property_type+"&LatestProperties="+latest+"&UnitArea="+area_property+"&Unit="+unit_property+"&school="+school+"&airport="+airport+"&transport="+transport+"&security="+security+"&shopping_mall="+malls+"&fuel="+fuel
 	#baseurl="https://www.aarz.pk/search/bot?postedBy=searchPage&view=&city_s="+city_names+"&price_min="+maximum_value+"&price_max=0estate_agent=&purpose=Sell&property_type="+property_type
 	if maximum_value == 0:
