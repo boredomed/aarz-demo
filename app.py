987966e32e1730e80281cbbb8a9b2b5d6f05873c
maryamzafar1 <30714111+maryamzafar1@users.noreply.github.com>
2017-08-24 04:16:21 +0500

    Update app.py

--- app.py
+++ app.py
@@ -376,10 +376,10 @@ def convertMaximum(pric, unit):
 	return str(price)
 
 def makeWebhookResult(data):
+	global locI
 	print("2locI: ",locI)
 	i=0
 	global row_title
-	global locI
 	length=len(data)
 	speech_data = "Here are some properties with your choice. We have total of "+str(length)+" records of your interest in city  "+city+"."
 	text_data = "Here are some properties with your choice. We have total of "+str(length)+" records of your interest in city  "+city+"."
