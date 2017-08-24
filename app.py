ffba2e9842347cefc04da3ca1ac1d63b92e98140
Muhammad Ahmad Ranan <mrana.bese15seecs@seecs.edu.pk>
2017-08-24 23:15:12 +0500

    buy_rent

--- app.py
+++ app.py
@@ -62,15 +62,22 @@ def processRequest(req):
 	s_id=processsession(req)
 	maximum_value=convertMaximum(maximum_valu, price_unit)
 	locI=processInnerLoc(req)
+	buy_rent=processPurpose(req)
 	print("LOCI: ",locI)
 	print(maximum_value)
 	print("this is session id:",s_id)
 	#baseurl = "https://aarz.pk/bot/index.php?city_name="+city_names+"&sector_name="+sector_names+"&minPrice="+maximum_value+"&type="+property_type+"&LatestProperties="+latest+"&UnitArea="+area_property+"&Unit="+unit_property+"&school="+school+"&airport="+airport+"&transport="+transport+"&security="+security+"&shopping_mall="+malls+"&fuel="+fuel
 	#baseurl="https://www.aarz.pk/search/bot?postedBy=searchPage&view=&city_s="+city_names+"&price_min="+maximum_value+"&price_max=0estate_agent=&purpose=Sell&property_type="+property_type
 	if maximum_value == 0:
-		baseurl="https://www.aarz.pk/search/bot?postedBy=searchPage&view=&city_s="+city_names+"&loc1="+locI+"&type="+property_type+"&land_area="+unit_property+"&min_r=0&max_r="+max_area
-	else:  
-		baseurl="https://www.aarz.pk/search/bot?postedBy=searchPage&view=&city_s="+city_names+"&loc1="+locI+"&type="+property_type+"&price_max="+maximum_value+"&land_area="+unit_property+"&min_r=0&max_r="+max_area
+		if buy_rent == 'a':
+			baseurl="https://www.aarz.pk/search/bot?postedBy=searchPage&view=&city_s="+city_names+"&loc1="+locI+"&type="+property_type+"&land_area="+unit_property+"&min_r=0&max_r="+max_area
+		else:
+			baseurl="https://www.aarz.pk/search/bot?postedBy=searchPage&view=&city_s="+city_names+"&loc1="+locI+"&type="+property_type+"&land_area="+unit_property+"&min_r=0&max_r="+max_area+"&purpose="+buy_rent
+	else:
+		if buy_rent == 'a':
+			baseurl="https://www.aarz.pk/search/bot?postedBy=searchPage&view=&city_s="+city_names+"&loc1="+locI+"&type="+property_type+"&price_max="+maximum_value+"&land_area="+unit_property+"&min_r=0&max_r="+max_area
+		else:
+			baseurl="https://www.aarz.pk/search/bot?postedBy=searchPage&view=&city_s="+city_names+"&loc1="+locI+"&type="+property_type+"&land_area="+unit_property+"&min_r=0&max_r="+max_area+"&purpose="+buy_rent
 	#print("city:",city_names)
 	print("url is:",baseurl)
 	result = urllib.request.urlopen(baseurl).read()
@@ -357,6 +364,12 @@ def processInnerLoc(req):
 	result = req.get("result")
 	parameters = result.get("parameters")
 	inner = parameters.get("inner_loc")
+	return inner
+
+def processPurpose(req):
+	result = req.get("result")
+	parameters = result.get("parameters")
+	inner = parameters.get("buy_rent")
 	return inner
 
 #Price
